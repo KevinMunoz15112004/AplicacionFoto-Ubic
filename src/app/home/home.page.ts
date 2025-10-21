548e3344dcd302d715d2e9dab479283960b368ec
KevinMunoz15112004 <kevinmunoz57898@gmail.com>
2025-10-20 19:44:00 -0500

    Apk

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, signal } from '@angular/core';
 import { FotoGeoService } from '../services/foto-geo-service';
 import { LocationService } from '../services/location';
 import { FotoGeo } from '../services/foto-geo-service';
+import { Preferences } from '@capacitor/preferences';
 import { Capacitor } from '@capacitor/core';
 import { Camera } from '@capacitor/camera';
 
@@ -23,10 +24,18 @@ export class HomePage implements OnInit {
     private fgService: FotoGeoService
   ) { }
 
-  
+
   async ngOnInit() {
     await this.loc.ensurePermissions();
     await this.fgService.pedirPermisosCamara();
+
+    const { value } = await Preferences.get({ key: 'ultima_foto_geo' });
+    if (value) {
+      const data: FotoGeo = JSON.parse(value);
+      this.fotoGeo.set(data);
+      this.latitude.set(data.lat);
+      this.longitude.set(data.lng);
+    }
   }
 
   async tomarFotoGeo() {
@@ -35,6 +44,11 @@ export class HomePage implements OnInit {
       this.fotoGeo.set(result);
       this.latitude.set(result.lat);
       this.longitude.set(result.lng);
+
+      await Preferences.set({
+        key: 'ultima_foto_geo',
+        value: JSON.stringify(result)
+      });
     } catch (err) {
       this.errorMsg.set('Error al tomar foto con ubicación');
       console.error(err);
